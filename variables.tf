cc8cadd3e6eed4c55eaefe5f3b789028d45f5d59
jlamenza <jlamenza@gmail.com>
2021-08-08 02:10:20 +0200

    change ami id

--- variables.tf
+++ variables.tf
@@ -1,10 +1,9 @@
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-04e931c820a9903e4"
-    us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
+    eu-west-1 = "ami-2d890a5a"
+    us-east-1 = "ami-8547fa98"
+  
   }
 }
 
